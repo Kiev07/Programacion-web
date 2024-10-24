e221da022b03decbca32c7c2281a9d79d892abdf
Kiev07 <warlokk07@gmail.com>
2024-10-24 16:43:09 -0600

    mmmmmmmmmmmmmmmmmmmmmmmmmmmm

---
+++ 24-10-2024/script.js
@@ -0,0 +1,43 @@
+//arreglo
+
+/*
+let array = ["dato1", 1,Boolean];
+console.log("tamaño array: " + array.length);
+array.push("Insert nuevo val");
+
+console.log(array[3]);
+
+console.log("nuevo tamaño array:" + array.length);
+console.log(array[3]);
+
+*/
+
+//Practica de 5*1 hasta 5*10
+
+
+/*
+let resultado = [];
+
+for ( let i = 0; i<= 10; i ++) {
+
+    let r = 5* i;
+        
+    if( r > 30) {
+        console.log(`Fin del arreglo, posición ${i}`);
+        break;
+    } 
+    resultado.push(`5 * ${i} = ${r}`);
+    console.log(resultado[i]);
+}
+ 
+
+*/
+
+
+
+
+
+
+
+
+
